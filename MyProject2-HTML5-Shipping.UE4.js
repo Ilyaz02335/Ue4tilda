c147d5d84be9300842af3cd2fe22923a27c41597
Ilyaz02335 <147247680+Ilyaz02335@users.noreply.github.com>
2023-11-13 16:11:55 +0700

    Update MyProject2-HTML5-Shipping.UE4.js

--- MyProject2-HTML5-Shipping.UE4.js
+++ MyProject2-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('UE4Game-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Ilyaz02335/Ue4tilda/d5c86ef0b7029837249c0fda649df47fc37fe6df/UE4Game-HTML5-Shipping.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
